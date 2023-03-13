3c5ed01bf53228d11f1bf783cc75416c7112928d
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-03-13 22:22:15 +0100

    Update function into common main to see changes into the two devices

--- ExampleApplication/shared/src/commonMain/kotlin/com/example/exampleapplication/Greeting.kt
+++ ExampleApplication/shared/src/commonMain/kotlin/com/example/exampleapplication/Greeting.kt
@@ -1,9 +1,17 @@
 package com.example.exampleapplication
 
+import kotlinx.datetime.*
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        return "Hello, Christmas is in ${daysUntilNewYear()} days!"
+    }
+
+    private fun daysUntilNewYear(): Int {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val closestNewYear = LocalDate(today.year, 12, 25)
+        return today.daysUntil(closestNewYear)
     }
 }
